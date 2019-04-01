4afaf1f0dd36568bb2430a1e3ffc9d8316e61c9f
lizecheng <zechengli2-c@ad.cityu.edu.hk>
2019-04-01 22:35:48 +0800

    main.cpp

---
+++ C++Project/main.cpp
@@ -0,0 +1,33 @@
+#include "User.h"
+#include <iostream>
+#include <string>
+#include "FileHandler.h"
+#include "LoanControl.h"
+using namespace std;
+
+int main() {
+	string str = readFile("123.txt");
+
+
+	std::istringstream iss(str);
+	std::string token;
+
+	/*User.foo("dsfsaf","dsa","sa","dsafds","asda");*/
+
+
+
+
+
+	while (std::getline(iss, token, '|'))
+	{
+		cout << token << endl;
+	}
+
+
+	/*
+		str = "Hello World!";
+		writeFile("123.txt", str);
+	*/
+	system("pause");
+	return 0;
+}
