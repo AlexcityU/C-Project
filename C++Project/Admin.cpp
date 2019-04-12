67e086abfb7584794ac1b2d6605961c46ed3a5ba
lizecheng <zechengli2-c@ad.cityu.edu.hk>
2019-04-12 13:29:44 +0800

    admin.cpp

--- C++Project/Admin.cpp
+++ C++Project/Admin.cpp
@@ -1,12 +1,13 @@
 #include "Admin.h"
 
+string admin_login::get_admin_id() const
 
-
-Admin::Admin()
 {
+	return admin_id;
 }
 
+string admin_login::get_admin_password() const
 
-Admin::~Admin()
 {
+	return admin_password;
 }
